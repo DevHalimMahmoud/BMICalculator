32fdcb8170b19e786dbd1aadf3a8c41022c605ce
abdomi7 <56141588+abdomi7@users.noreply.github.com>
2021-08-29 01:24:12 +0200

    Implementing Splash Screen With Animations

--- app/src/main/java/com/example/bmicalculator/ui/screens/GenderScreen.kt
+++ app/src/main/java/com/example/bmicalculator/ui/screens/GenderScreen.kt
@@ -14,9 +14,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 
 @Composable
@@ -152,4 +154,10 @@ fun GenderScreenContent(navController: NavController) {
     }
 
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+        GenderScreen(navController = rememberNavController())
 }
